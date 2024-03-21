7288a36fecd7b54901d6847e6046a5cd48cbad75
Jorge Matos <jisapjisap@gmail.com>
2024-03-21 11:32:56 +0100

    setup

---
+++ app/buttons/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>Primary</Button>
+      <Button>Primary Outline</Button>
+    </div>  
+  )
+}
+
+export default ButtonsPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500 font-bold">
-        Hello Lingo
-      </p>
+      <Button size="lg">
+        Click me
+      </Button>
     </div>
   );
 }
