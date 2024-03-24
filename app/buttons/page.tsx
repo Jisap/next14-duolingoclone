a9733e421c88ec503547f2fca3830d705b93e600
Jorge Matos <jisapjisap@gmail.com>
2024-03-24 11:00:37 +0100

    buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -3,8 +3,34 @@ import { Button } from "@/components/ui/button"
 const ButtonsPage = () => {
   return (
     <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
-      <Button>Primary</Button>
-      <Button>Primary Outline</Button>
+      <Button>Default</Button>
+      <Button variant="primary">
+        Primary
+      </Button>
+      <Button variant="primaryOutline">
+        Primary Outline
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="secondaryOutline">
+        Secondary Outline
+      </Button>
+      <Button variant="danger">
+        Secondary
+      </Button>
+      <Button variant="dangerOutline">
+        Secondary Outline
+      </Button>
+      <Button variant="super">
+        Super
+      </Button>
+      <Button variant="superOutline">
+        Super Outline
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
     </div>  
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,18 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg">
-        Click me
+      <Button>Default</Button>
+      <Button variant="primary">
+        Primary
+      </Button>
+      <Button variant="primaryOutline">
+        Primary Outline
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="secondaryOutline">
+        Secondary Outline
       </Button>
     </div>
   );
