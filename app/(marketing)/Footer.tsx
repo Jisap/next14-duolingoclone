ba7286f6299ad0e67b4eb6ec3d7bcdc0f7a58193
Jorge Matos <jisapjisap@gmail.com>
2024-03-28 20:11:17 +0100

    footer

--- app/(marketing)/Footer.tsx
+++ app/(marketing)/Footer.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 
 
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image 
+            src="/hr.svg"
+            height={32}
+            width={40}
+            alt="Croatian"
+          />
+        </Button>
+      </div>
     </footer>  
   )
 }
