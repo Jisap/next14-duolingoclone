f727cfb12b2eaf3cf723cef00bd1d1906cdd7ead
Jorge Matos <jisapjisap@gmail.com>
2024-03-29 16:43:06 +0100

    sidebar-item

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,13 @@
 
 
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SidebarItem } from './Sidebar-Item';
+import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+
+
 
 type Props = {
   className?: string;
@@ -9,10 +16,54 @@ type Props = {
 export const Sidebar = ({className}: Props) => {
   return (
     <div className={cn(
-      "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+      "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
       className
     )}>
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="/mascot.svg"
+            height={40}
+            width={40}
+            alt="Mascot"
+          />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+
+      <div className='flex flex-col gap-y-2 flex-1'>
+        <SidebarItem 
+          label="Learn"
+          href="/learn"
+          iconSrc="/learn.svg"
+        />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem
+          label="quests"
+          href="/quests"
+          iconSrc="/quests.svg"
+        />
+        <SidebarItem
+          label="shop"
+          href="/shop"
+          iconSrc="/shop.svg"
+        />
+      </div>
+
+      <div className='p-4'>
+        <ClerkLoading>
+          <Loader className='h-5 w-5 text-muted-foreground animate-spin' />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton  afterSignOutUrl='/'/>
+        </ClerkLoaded>
+      </div>
     </div>
   )
 }
