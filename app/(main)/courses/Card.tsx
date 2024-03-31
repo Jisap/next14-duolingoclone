ba1da7fdcd82bbbfab8bad3f8bc8871b0c52a7b0
Jorge Matos <jisapjisap@gmail.com>
2024-03-31 17:08:46 +0200

    tablas y relaciones

--- app/(main)/courses/Card.tsx
+++ app/(main)/courses/Card.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
+import Image from "next/image";
 
 
 type Props = {
@@ -35,6 +36,16 @@ const Card = ({
           </div>
         )}
       </div>
+      <Image
+        src={imageSrc}
+        alt={title}
+        height={70}
+        width={93.33}
+        className="rounded-lg drop-shadow-md border object-cover"
+      />
+      <p className="text-neutral-700 text-center font-bold mt-3">
+        {title}
+      </p>
     </div>
   )
 }
--- db/schema.ts
+++ db/schema.ts
@@ -6,3 +6,104 @@ export const courses = pgTable("courses", {
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+export const units = pgTable("units", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(), // Unit 1
+  description: text("description").notNull(), // Learn the basics of spanish
+  courseId: integer("course_id").references(() => courses.id, { onDelete: "cascade" }).notNull(),
+  order: integer("order").notNull(),
+});
+
+export const unitsRelations = relations(units, ({ many, one }) => ({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+  lessons: many(lessons),
+}));
+
+export const lessons = pgTable("lessons", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  unitId: integer("unit_id").references(() => units.id, { onDelete: "cascade" }).notNull(),
+  order: integer("order").notNull(),
+});
+
+export const lessonsRelations = relations(lessons, ({ one, many }) => ({
+  unit: one(units, {
+    fields: [lessons.unitId],
+    references: [units.id],
+  }),
+  challenges: many(challenges),
+}));
+
+export const challengesEnum = pgEnum("type", ["SELECT", "ASSIST"]);
+
+export const challenges = pgTable("challenges", {
+  id: serial("id").primaryKey(),
+  lessonId: integer("lesson_id").references(() => lessons.id, { onDelete: "cascade" }).notNull(),
+  type: challengesEnum("type").notNull(),
+  question: text("question").notNull(),
+  order: integer("order").notNull(),
+});
+
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
+  lesson: one(lessons, {
+    fields: [challenges.lessonId],
+    references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
+}));
+
+export const challengeOptions = pgTable("challenge_options", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
+  text: text("text").notNull(),
+  correct: boolean("correct").notNull(),
+  imageSrc: text("image_src"),
+  audioSrc: text("audio_src"),
+});
+
+export const challengeOptionsRelations = relations(challengeOptions, ({ one }) => ({
+  challenge: one(challenges, {
+    fields: [challengeOptions.challengeId],
+    references: [challenges.id],
+  }),
+}));
+
+export const challengeProgress = pgTable("challenge_progress", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
+  completed: boolean("completed").notNull().default(false),
+});
+
+export const challengeProgressRelations = relations(challengeProgress, ({ one }) => ({
+  challenge: one(challenges, {
+    fields: [challengeProgress.challengeId],
+    references: [challenges.id],
+  }),
+}));
+
+export const coursesRelations = relations(courses, ({ many }) => ({ // Establece relaciones entre la tabla de cursos y otras tablas en la base de datos.
+  userProgress: many(userProgress), // relación de uno a muchos ( un curso pueden tener muchos usuarios aprendiendo)
+  units: many(units),               // relación de uno a muchos ( un curso pueden tener muchas unidades )    
+}));
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade" }), // relación con courses.id. Si se elimina un course se elimina aquí también.
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(0),
+});
+
+export const userProgressRelations = relations(userProgress, ({ one }) => ({ // Relación entre userProgress y otras tablas (activeCourse es el nombre de la relación)
+  activeCourse: one(courses, {              // relación de uno a uno (userProgress solo puede tener un course activo)
+    fields: [userProgress.activeCourseId],  // el campo activeCourseId de userProgress será referenciado con courses.id de la tabla de courses
+    references: [courses.id],
+  }),
+}));
