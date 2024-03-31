8cffb978c528c1ad2dc6940d7f603402a900a4c9
Jorge Matos <jisapjisap@gmail.com>
2024-03-31 17:38:00 +0200

    querys

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,10 +1,17 @@
-import { getCourses } from "@/db/queries"
+import { getCourses, getUserProgress } from "@/db/queries"
 import List from "./List"
+import { userProgress } from '../../../db/schema';
 
 
 const CoursesPage = async() => {
 
-  const courses = await getCourses()
+  const coursesData = getCourses();
+  const userProgressData = getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData
+  ])
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
@@ -13,7 +20,7 @@ const CoursesPage = async() => {
       </h1>
       <List 
         courses={courses} 
-        activeCourseId={1}
+        activeCourseId={userProgress?.activeCourseId}
       />
     </div>
   )
--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,25 @@
 import { cache } from "react";
 import { db } from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "./schema";
+
+
+export const getUserProgress = cache(async() => {
+  const {userId} = await auth()
+  if(!userId){
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId), // Se busca en userProgress el userId que coincida con userId logueado
+    with: {
+      activeCourse: true // Aquí, se solicita que la relación activeCourse también se cargue y esté disponible en el objeto data resultante.
+    },
+  });
+
+  return data;
+})
 
 
 export const getCourses = cache(async () => {
