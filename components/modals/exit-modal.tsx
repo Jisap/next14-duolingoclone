150efb739c210c6a457066156a4a9b193fe18152
Jorge Matos <jisapjisap@gmail.com>
2024-04-08 20:17:32 +0200

    exit-modal

---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useExitModal } from "@/store/use-exit-modal";
+
+const ExitModal = () => {
+
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false); // False indica que el componente esta en el lado del servidor
+  const { isOpen, close } = useExitModal();
+
+  useEffect(() => setIsClient(true), []); // true indica que el componente esta en lado del cliente
+
+  if (!isClient) {
+    return null;
+  }
+
+
+  return (
+    
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center w-full justify-center mb-5">
+            <Image
+              src="/mascot_sad.svg"
+              alt="Mascot"
+              height={80}
+              width={80}
+            />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            Wait, don&apos;t go!
+          </DialogTitle>
+          <DialogDescription className="text-center text-base">
+            You&apos;re about to leave the lesson. Are you sure?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-4">
+          <div className="flex flex-col gap-y-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              Keep learning
+            </Button>
+            <Button
+              variant="dangerOutline"
+              className="w-full"
+              size="lg"
+              onClick={() => {
+                close();
+                router.push("/learn");
+              }}
+            >
+              End session
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+  
+}
+
+export default ExitModal
