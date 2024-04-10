0caa945d3c55ad5ef5becc90bb5bce43978a68b2
Jorge Matos <jisapjisap@gmail.com>
2024-04-10 19:44:00 +0200

    card

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -32,7 +32,29 @@ const Card: FC<CardProps> = ({
   selected,
 }) => {
   return (
-    <div>Card</div>
+    <div 
+      onClick={() => {}}
+      className={cn(
+        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+        selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
+        selected && status === "correct" 
+          && "border-green-300 bg-green-100 hover:bg-green-100",
+        selected && status === "wrong" 
+          && "border-rose-300 bg-rose-100 hover:bg-rose-100",
+        disabled && "pointer-events-none hover:bg-white",
+        type === "ASSIST" && "lg:p-3 w-full"
+      )}  
+    >
+      {imgSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image 
+            src={imgSrc} 
+            alt={text} 
+            fill 
+          />
+        </div>
+      )}
+    </div>
   )
 }
 
