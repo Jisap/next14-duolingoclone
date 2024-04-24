fc680655397af5456e8977842eea4ad566d13747
Jorge Matos <jisapjisap@gmail.com>
2024-04-24 19:07:17 +0200

    Quests

---
+++ app/(main)/quests/page.tsx
@@ -0,0 +1,99 @@
+import { StickyWrapper } from "@/components/sticky-wrapper"
+import { UserProgress } from "@/components/user-progress"
+import { getUserProgress, getUserSubscription } from "@/db/queries"
+import { redirect } from "next/navigation";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import Image from "next/image";
+import { Promo } from "@/components/Promo";
+import { Quests } from "@/components/Quest";
+import { Progress } from "@/components/ui/progress";
+import { quests } from "@/constants.ts";
+
+
+export const metadata = {
+  title: "Lingo | Quests page",
+  description: "Complete quests by earning points.",
+};
+
+const QuestPage = async() => {
+
+  const userProgressData = getUserProgress();         // Se busca en userProgress el userId que coincida con userId logueado
+  const userSubscriptionData = getUserSubscription(); // Se busca en bd el userSubscription correspondiente al usuario logueado
+
+
+  const [ 
+    userProgress,
+    userSubscription,
+  ] = await Promise.all([
+    userProgressData,
+    userSubscriptionData,
+  ]);
+
+  if(!userProgress || !userProgress.activeCourse){
+    redirect("/courses")
+  }
+
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress 
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={!!userSubscription?.isActive}
+        />
+        
+        {!userSubscription?.isActive && <Promo />}
+      </StickyWrapper>
+
+      <FeedWrapper>
+        <div className="w-full flex flex-col items-center">
+          <Image
+            src="/quests.svg"
+            alt="Quest"
+            height={90}
+            width={90}
+          />
+          <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+            Quest
+          </h1>
+          <p className="text-muted-foreground text-center text-lg mb-6">
+            Complete quests by earning points.
+          </p>
+          
+          <ul className="w-full">
+            {quests.map((quest, index) => {
+
+              const progress = (userProgress.points / quest.value) * 100;
+
+              return (
+                <li
+                  key={quest.title}
+                  className="flex items-center w-full gap-x-4 border-t-2 p-4"
+                >
+                  <Image
+                    src="/points.svg"
+                    alt="Points"
+                    height={60}
+                    width={60}
+                  />
+
+                  <div className="flex flex-col gap-y-2 w-full">
+                    <p className="text-neutral-700 text-xl font-bold">
+                      {quest.title}
+                    </p>
+
+                    <Progress value={progress} className="h-3" />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+
+        </div>
+      </FeedWrapper>
+    </div>
+  )
+}
+
+export default QuestPage
